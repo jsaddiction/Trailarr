03995fbc3df37dfae8afc890710aab325b87c0e0
jsaddiction <04reduramax@gmail.com>
2024-03-11 20:39:01 -0400

    Refactor CLI argument handling and logging configuration

--- trailarr_cli.py
+++ trailarr_cli.py
@@ -1,14 +1,26 @@
 """CLI Interface for Trailarr"""
 
 import logging
+from dataclasses import dataclass
 from argparse import ArgumentParser
 from src import __app_name__, __description__, __version__
 from trailarr import TrailArr, CFG
 
 app = TrailArr()
+log = logging.getLogger("Trailarr.CLI")
 
 
-def get_arguments():
+@dataclass
+class CLIConfig:
+    """Trailarr CLI Configuration"""
+
+    tmdb: int = None
+    all: bool = False
+    quiet: bool = False
+    parser: ArgumentParser = None
+
+
+def get_arguments() -> CLIConfig:
     """Collect arguments passed by user"""
     parser = ArgumentParser(prog=__app_name__, description=__description__)
     parser.add_argument(
@@ -16,28 +28,45 @@ def get_arguments():
     )
     parser.add_argument("-tmdb", metavar="TMDB id", dest="tmdb", help="TMDB id of the movie", type=int, default=None)
     parser.add_argument("-all", action="store_true", dest="all", help="Process all movies in Radarr", default=False)
+    parser.add_argument("-quiet", action="store_true", dest="quiet", help="Suppress console output", default=False)
 
-    return parser.parse_args()
+    cfg = parser.parse_args()
+    return CLIConfig(tmdb=cfg.tmdb, all=cfg.all, quiet=cfg.quiet, parser=parser)
+
+
+def config_logging(quiet: bool = False):
+    """Configure logging for console mode"""
+    console_level = 100 if quiet else CFG.log_level.upper()
+    for handler in logging.getLogger().handlers:
+        # Disable file logging
+        if handler.name == "file":
+            handler.setLevel(100)
+
+        # Set console logging level / disabled if quiet was set
+        elif handler.name == "console":
+            handler.setLevel(console_level)
 
 
 def main():
     """Run the application"""
-    # Set console logger to the log level in the config
-    for handler in logging.getLogger().handlers:
-        if handler.name == "console":
-            handler.setLevel(CFG.log_level.upper())
-            break
-
+    # Get arguments and config logging
     args = get_arguments()
-    if args.all:
+    config_logging(args.quiet)
+
+    # Process user request
+    if args.all and args.tmdb is None:
         app.process_all()
 
-    elif args.tmdb:
+    elif args.tmdb and not args.all:
         movie = app.radarr.get_movie_by_id(args.tmdb)
         if not movie:
             print(f"TMDB id {args.tmdb} not found in Radarr.")
             return
         app.process_movie(movie)
+
+    else:
+        log.warning("You must specify a TMDB id or use the -all flag.")
+        args.parser.print_help()
 
 
 if __name__ == "__main__":
